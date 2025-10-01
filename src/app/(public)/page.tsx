f97c3b5878d888d03156e09981e2fd247f626bcb
Sadia492 <sadiaafrin2792@gmail.com>
2025-10-02 00:36:38 +0600

    folder structure added

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
+      <Button>Default</Button>
+    </div>
+  );
+}
